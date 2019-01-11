4b9a44a688bd99f8fd5ab331bb744124cf074f17
Kub666 <jakub.zdzieblo@gmail.com>
2019-01-11 15:33:50 +0100

    app z zajęć

---
+++ js/app_example.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let dateEl = document.getElementById("date");
+
+    $.ajax({
+        url: "http://date.jsontest.com",
+        dataType: "json"
+    })
+        .done(function (data) {
+            dateEl.innerText = data.date+" "+data.time;
+        });
+
+
+    $.ajax({
+        url: "https://swapi.co/api/people/4/",
+        dataType: "json"
+    })
+        .done(function(person){
+            addSwPersonElement(person);
+        });
+
+})
+
+function addSwPersonElement(personData){
+    let parentElem = document.getElementById("sw");
+
+    let headerEl = document.createElement("h2");
+    headerEl.innerText = personData.name
+
+    let listEl = document.createElement("ul");
+
+    let hairEl = document.createElement("li");
+    hairEl.innerText = "hair: "+personData["hair_color"];
+
+    let heightEl = document.createElement("li");
+    heightEl.innerText = "height "+personData.height;
+
+    listEl.appendChild(hairEl);
+    listEl.appendChild(heightEl);
+
+    parentElem.appendChild(headerEl);
+    parentElem.appendChild(listEl);
+
+
+}
